dada1c200612ea2c0663671e2906b28ff6edfddf
Juan Ara <juan@juanylaura.es>
2020-09-12 23:21:22 +0200

    Add QuickEmotes

---
+++ SavedVariables/QuickEmotes.lua
@@ -0,0 +1,19 @@
+QuickEmotes_SV =
+{
+    ["EU Megaserver"] = 
+    {
+        ["@cbr600f"] = 
+        {
+            ["$AccountWide"] = 
+            {
+                ["Options"] = 
+                {
+                    ["button_position_x"] = 0,
+                    ["showing_submenu_delay"] = 200,
+                    ["disable_cur_mod_select"] = false,
+                    ["version"] = 0.1000000000,
+                },
+            },
+        },
+    },
+}
