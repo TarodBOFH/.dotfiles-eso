ef420f92dbdc9aac6236be44d2f90837d40c762f
Juan Ara <juan@juanylaura.es>
2020-09-12 23:20:10 +0200

    Add AlphaGear, AlphaStyle and ChampionPoints

---
+++ SavedVariables/AlphaStyle.lua
@@ -0,0 +1,226 @@
+AlphaStyle_Settings =
+{
+    ["Default"] = 
+    {
+        ["@cbr600f"] = 
+        {
+            ["$AccountWide"] = 
+            {
+                ["version"] = "1",
+            },
+        },
+    },
+}
+AlphaStyle_Styles =
+{
+    ["Default"] = 
+    {
+        ["@cbr600f"] = 
+        {
+            ["Tarod Ararthin"] = 
+            {
+                ["Styles"] = 
+                {
+                    [1] = 
+                    {
+                        ["OutfitId"] = -1,
+                        ["Name"] = "Covenant",
+                        ["Collectibles"] = 
+                        {
+                            [17] = 0,
+                            [2] = 365,
+                            [3] = 0,
+                            [4] = 249,
+                            [18] = 0,
+                            [9] = 379,
+                            [10] = 413,
+                            [11] = 0,
+                            [12] = 0,
+                            [13] = 0,
+                            [14] = 0,
+                            [15] = 0,
+                        },
+                        ["TitleId"] = 1,
+                        ["Costume"] = 
+                        {
+                            ["id"] = "4618645115282536684",
+                            ["link"] = "|H0:item:55262:4:1:0:0:0:0:0:0:0:0:0:0:0:1:0:1:1:0:0:619824|h|h",
+                        },
+                        ["IgnoreTitle"] = false,
+                        ["SortKey"] = "New Style 1",
+                    },
+                    [2] = 
+                    {
+                        ["OutfitId"] = -1,
+                        ["Name"] = "No Style",
+                        ["Collectibles"] = 
+                        {
+                            [17] = 0,
+                            [2] = 55,
+                            [3] = 0,
+                            [4] = 0,
+                            [18] = 0,
+                            [9] = 379,
+                            [10] = 0,
+                            [11] = 0,
+                            [12] = 0,
+                            [13] = 0,
+                            [14] = 0,
+                            [15] = 0,
+                        },
+                        ["TitleId"] = 25,
+                        ["Costume"] = 
+                        {
+                            ["id"] = "4618645115282536684",
+                            ["link"] = "|H0:item:55262:4:1:0:0:0:0:0:0:0:0:0:0:0:1:0:1:1:0:0:619824|h|h",
+                        },
+                        ["IgnoreTitle"] = false,
+                        ["SortKey"] = "New Style 2",
+                    },
+                    [3] = 
+                    {
+                        ["OutfitId"] = 3,
+                        ["Name"] = "PvP Dark Hojablanca",
+                        ["Collectibles"] = 
+                        {
+                            [17] = 0,
+                            [2] = 55,
+                            [3] = 0,
+                            [4] = 0,
+                            [18] = 0,
+                            [9] = 379,
+                            [10] = 0,
+                            [11] = 0,
+                            [12] = 0,
+                            [13] = 0,
+                            [14] = 0,
+                            [15] = 0,
+                        },
+                        ["TitleId"] = 38,
+                        ["Costume"] = 
+                        {
+                            ["id"] = "4618645115282536684",
+                            ["link"] = "|H0:item:55262:4:1:0:0:0:0:0:0:0:0:0:0:0:1:0:1:1:0:0:619824|h|h",
+                        },
+                        ["IgnoreTitle"] = false,
+                        ["SortKey"] = "New Style 3",
+                    },
+                    [4] = 
+                    {
+                        ["OutfitId"] = 3,
+                        ["Name"] = "PvP Dark",
+                        ["Collectibles"] = 
+                        {
+                            [17] = 0,
+                            [2] = 55,
+                            [3] = 0,
+                            [4] = 0,
+                            [18] = 0,
+                            [9] = 379,
+                            [10] = 0,
+                            [11] = 0,
+                            [12] = 0,
+                            [13] = 0,
+                            [14] = 0,
+                            [15] = 0,
+                        },
+                        ["TitleId"] = 38,
+                        ["Costume"] = 
+                        {
+                            ["id"] = 0,
+                            ["link"] = 0,
+                        },
+                        ["IgnoreTitle"] = false,
+                        ["SortKey"] = "New Style 4",
+                    },
+                    [5] = 
+                    {
+                        ["OutfitId"] = 1,
+                        ["Name"] = "PvE Akelarre",
+                        ["Collectibles"] = 
+                        {
+                            [17] = 0,
+                            [2] = 55,
+                            [3] = 0,
+                            [4] = 0,
+                            [18] = 0,
+                            [9] = 379,
+                            [10] = 0,
+                            [11] = 0,
+                            [12] = 0,
+                            [13] = 0,
+                            [14] = 0,
+                            [15] = 0,
+                        },
+                        ["TitleId"] = 42,
+                        ["Costume"] = 
+                        {
+                            ["id"] = "4618645115282406501",
+                            ["link"] = "|H0:item:55262:4:1:0:0:0:0:0:0:0:0:0:0:0:1:0:1:1:0:0:15995|h|h",
+                        },
+                        ["IgnoreTitle"] = false,
+                        ["SortKey"] = "New Style 5",
+                    },
+                },
+                ["LastId"] = 5,
+                ["version"] = "1",
+            },
+            ["Lord Tarod of Aragon"] = 
+            {
+                ["Styles"] = 
+                {
+                },
+                ["LastId"] = 0,
+                ["version"] = "1",
+            },
+            ["Neria Rothalen"] = 
+            {
+                ["Styles"] = 
+                {
+                },
+                ["LastId"] = 0,
+                ["version"] = "1",
+            },
+            ["Faulcon Delacy"] = 
+            {
+                ["Styles"] = 
+                {
+                },
+                ["LastId"] = 0,
+                ["version"] = "1",
+            },
+            ["Jimeneka Ararthin"] = 
+            {
+                ["Styles"] = 
+                {
+                },
+                ["LastId"] = 0,
+                ["version"] = "1",
+            },
+            ["Sneaks-Behind"] = 
+            {
+                ["Styles"] = 
+                {
+                },
+                ["LastId"] = 0,
+                ["version"] = "1",
+            },
+            ["Jimenuki Ararthin"] = 
+            {
+                ["Styles"] = 
+                {
+                },
+                ["LastId"] = 0,
+                ["version"] = "1",
+            },
+            ["Tarodion Ararthin"] = 
+            {
+                ["Styles"] = 
+                {
+                },
+                ["LastId"] = 0,
+                ["version"] = "1",
+            },
+        },
+    },
+}
