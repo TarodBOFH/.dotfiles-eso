0fba332bc6b9060ab49c41aa4c8f47a5b0422ae5
Juan Ara <juan@juanylaura.es>
2020-11-02 20:05:20 +0100

    Added Tamriel Trade Centre

---
+++ SavedVariables/TamrielTradeCentre.lua
@@ -0,0 +1,86 @@
+TamrielTradeCentreVars =
+{
+    ["Default"] = 
+    {
+        ["@cbr600f"] = 
+        {
+            ["$AccountWide"] = 
+            {
+                ["NAData"] = 
+                {
+                    ["AutoRecordEntries"] = 
+                    {
+                        ["Guilds"] = 
+                        {
+                        },
+                        ["Count"] = 0,
+                    },
+                    ["IsFirstExecute"] = true,
+                    ["Guilds"] = 
+                    {
+                    },
+                },
+                ["EUData"] = 
+                {
+                    ["AutoRecordEntries"] = 
+                    {
+                        ["Guilds"] = 
+                        {
+                        },
+                        ["Count"] = 0,
+                    },
+                    ["IsFirstExecute"] = false,
+                    ["Guilds"] = 
+                    {
+                        ["Hojablanca"] = 
+                        {
+                            ["LastUpdate"] = 1604307070,
+                            ["Entries"] = 
+                            {
+                                [0] = 
+                                {
+                                    ["ItemType"] = 2,
+                                    ["Amount"] = 1,
+                                    ["Level"] = 1,
+                                    ["TotalPrice"] = 500,
+                                    ["Name"] = "Rawhide Guards",
+                                    ["Category2IDOverWrite"] = 3,
+                                    ["ItemLink"] = "|H0:item:56057:30:1:0:0:0:0:0:0:0:0:0:0:0:0:1:1:0:0:10000:0|h|h",
+                                    ["UID"] = "4759967956141277290",
+                                    ["ID"] = 3103,
+                                    ["QualityID"] = 0,
+                                    ["TraitID"] = 14,
+                                },
+                            },
+                            ["LastFullScan"] = 1603959949,
+                        },
+                    },
+                },
+                ["ClientCulture"] = "en",
+                ["ActualVersion"] = 10,
+                ["Settings"] = 
+                {
+                    ["EnableItemToolTipPricing"] = true,
+                    ["EnableToolTipAggregate"] = true,
+                    ["EnableItemPriceToChatBtn"] = true,
+                    ["EnableToolTipLastUpdate"] = true,
+                    ["EnableSelfEntriesUpload"] = true,
+                    ["EnablePriceToChatStat"] = true,
+                    ["EnableItemSoldNotification"] = true,
+                    ["EnableToolTipStat"] = true,
+                    ["EnableAutoRecordStoreEntries"] = true,
+                    ["EnableItemSearchOnlineBtn"] = true,
+                    ["AdditionalPriceToChatLang"] = 
+                    {
+                    },
+                    ["EnablePriceToChatSuggested"] = true,
+                    ["EnableToolTipSuggested"] = true,
+                    ["EnablePriceToChatAggregate"] = false,
+                    ["EnablePriceToChatLastUpdate"] = false,
+                    ["MaxAutoRecordStoreEntryCount"] = 20000,
+                },
+                ["version"] = 3,
+            },
+        },
+    },
+}
