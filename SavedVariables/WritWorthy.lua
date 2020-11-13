e4bb471a7077f044999d6673f802557e4d110424
Juan Ara <juan@juanylaura.es>
2020-11-13 16:42:01 +0100

    Add Writ Worthy

---
+++ SavedVariables/WritWorthy.lua
@@ -0,0 +1,105 @@
+WritWorthyVars =
+{
+    ["Default"] = 
+    {
+        ["@cbr600f"] = 
+        {
+            ["Sneaks-Behind"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Jimenuki Ararthin"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Tarodion Ararthin"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["$AccountWide"] = 
+            {
+                ["enable_station_colors"] = true,
+                ["version"] = 1,
+                ["position"] = 
+                {
+                    [4] = 686,
+                    [1] = 35,
+                    [2] = 136,
+                    [3] = 1160,
+                },
+            },
+            ["Tharodriel Ararthin"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Neria Rothalen"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Tarodio Ararthinus"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Cassus Andronicus"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Tarod Ararthin"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Faulcon Delacy"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Jimeneka Ararthin"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Lord Tarod of Aragon"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+            ["Tarolog Agharghim"] = 
+            {
+                ["version"] = 1,
+                ["writ_unique_id"] = 
+                {
+                },
+            },
+        },
+    },
+}
